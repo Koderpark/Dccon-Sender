bc82bc8e3926eceea038595e6aeaad1cdcdced52
박성훈 / Koder <koder0205@gmail.com>
2020-04-30 14:14:03 +0900

    update
    +a

---
+++ backend v3/js/index.js
@@ -0,0 +1,8 @@
+function popup(){
+    document.getElementById('grey').style.display = "block";
+    document.getElementById('popup').style.display = "block";
+}
+function exit(){
+    document.getElementById('grey').style.display = "none";
+    document.getElementById('popup').style.display = "none";
+}
